09301b2b941faa60c8778e1fe67beceb37d734f0
Daikiinstitucional <lucas.hori989@al.unieduk.com.br>
2024-09-01 21:58:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/home')
 def home():
     return render_template("index.html")
 
